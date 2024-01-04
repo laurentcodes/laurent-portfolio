4ebefd959d47e91e844fd80001693450244c1bb8
Saint Laurent <stlaurentcodes@gmail.com>
2024-01-04 22:31:18 +0100

    fix: revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
 
 import react from '@astrojs/react';
 
@@ -8,5 +7,4 @@ import react from '@astrojs/react';
 export default defineConfig({
 	integrations: [tailwind(), react()],
 	output: 'server',
-	adapter: netlify(),
 });
