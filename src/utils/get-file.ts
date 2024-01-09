518de599c81bd5d818180366ef469fbf9782b388
Saint Laurent <stlaurentcodes@gmail.com>
2024-01-09 15:15:36 +0100

    fix: minor fixes

--- src/utils/get-file.ts
+++ src/utils/get-file.ts
@@ -1,7 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
-export function getFile(fileName: string) {
+export function getPosts() {
+	return fs.readdirSync(path.join(process.cwd(), 'content', 'posts'));
+}
+
+export function getPost(fileName: string) {
 	return fs.readFileSync(
 		path.join(process.cwd(), 'content', 'posts', fileName + '.md'),
 		'utf-8'
