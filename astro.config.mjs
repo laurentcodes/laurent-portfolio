cf3f9553db953bf8df36e02fdb410685c8cefa96
Laurent <o.sherkes@gmail.com>
2024-01-19 12:02:17 +0100

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,21 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
-
 import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://laurentcodes.xyz',
 	integrations: [
 		tailwind({
 			nesting: true
 		}),
 		react(),
-		partytown()
+		partytown(),
+		sitemap()
 	],
 	output: 'server',
 	adapter: netlify()
 });
+
